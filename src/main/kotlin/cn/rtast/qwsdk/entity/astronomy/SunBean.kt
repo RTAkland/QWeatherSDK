ba4ef2f2faab7b6e7838f473dafcca7e3505f0a3
RTAkland <markushammered@gmail.com>
2023-01-25 14:18:57 +0800

    add new entities

---
+++ src/main/kotlin/cn/rtast/qwsdk/entity/astronomy/SunBean.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2023 RTAkland
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package cn.rtast.qwsdk.entity.astronomy
+
+import cn.rtast.qwsdk.entity.Refer
+
+data class SunBean(
+    val code: String,
+    val fxLink: String,
+    val refer: Refer,
+    val sunrise: String,
+    val sunset: String,
+    val updateTime: String
+)
