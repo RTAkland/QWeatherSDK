dba6fb91bd23f6fd9d92f312fd1452264b9e17fb
RTAkland <markushammered@gmail.com>
2023-01-29 02:24:55 +0800

    add: 添加公共类

---
+++ src/main/kotlin/cn/rtast/qwsdk/Global.kt
@@ -0,0 +1,7 @@
+package cn.rtast.qwsdk
+
+object Global {
+    var key: String? = null
+    var rootAPI = "https://devapi.qweather.com/v7"
+    const val geoAPI = "https://geoapi.qweather.com/v2"
+}
