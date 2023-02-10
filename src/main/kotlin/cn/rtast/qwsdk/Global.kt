aea56575be6436502e7e205579bdaf5c3dc87403
RTAkland <markushammered@gmail.com>
2023-02-10 17:26:20 +0800

    update: 更新版本号

--- src/main/kotlin/cn/rtast/qwsdk/Global.kt
+++
@@ -1,7 +0,0 @@
-package cn.rtast.qwsdk
-
-object Global {
-    var key: String? = null
-    var rootAPI = "https://devapi.qweather.com/v7"
-    const val geoAPI = "https://geoapi.qweather.com/v2"
-}
